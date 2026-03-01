d40d1cd510478d5d647e4554804e43dd1a884456
Levy Nunes <levy.vix@gmail.com>
2026-03-01 03:25:43 -0300

    chore: set pyright typeCheckingMode to basic

--- .config/nvim/lua/plugins/disable-pyright.lua
+++ .config/nvim/lua/plugins/disable-pyright.lua
@@ -7,7 +7,7 @@ return {
           settings = {
             python = {
               analysis = {
-                typeCheckingMode = "standard",
+                typeCheckingMode = "basic",
               },
             },
           },
